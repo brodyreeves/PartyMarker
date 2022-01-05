1de6d20aeaf2867c6e7e2bc4d12812a00ce289d7
Brody Reeves <brodyreeves@gmail.com>
2022-01-04 23:13:12 -0700

    Revert to publishing a single version
    Publishing all at once had issues with bcc applying to all versions

--- publish.sh
+++ publish.sh
@@ -8,10 +8,7 @@ if [ $# -eq 0 ] || [ $# -gt 1 ]; then
 fi
 
 # check first argument (tag name) is valid - v#.#.#-retail|classic|bcc
-# if ! [[ "$1" =~ ^(v[1-9][0-9]{0,2})(\.[0-9]{1,3}){2,3}-(retail|classic|bcc)$ ]]; then
-
-# check first argument (tag name) is valid - v#.#.#
-if ! [[ "$1" =~ ^(v[1-9][0-9]{0,2})(\.[0-9]{1,3}){2,3}$ ]]; then
+if ! [[ "$1" =~ ^(v[1-9][0-9]{0,2})(\.[0-9]{1,3}){2,3}-(retail|classic|bcc)$ ]]; then
     echo "Invalid tag name"
     exit
 fi
@@ -28,7 +25,7 @@ fi
 
 echo "Checking for remote tag existence"
 
-result=$(($(curl -s "https://api.github.com/repos/brodyreeves/PartyMarker/tags" | grep -Po '"name":.*?[^\\]",' | grep -cP "\"$1\"")+$(curl -s "https://api.github.com/repos/brodyreeves/PartyMarker/tags" | grep -Po '"name":.*?[^\\]",' | grep -cP "\"$1-classic\"")+$(curl -s "https://api.github.com/repos/brodyreeves/PartyMarker/tags" | grep -Po '"name":.*?[^\\]",' | grep -cP "\"$1-bcc\"")))
+result=$(curl -s "https://api.github.com/repos/brodyreeves/PartyMarker/tags" | grep -Po '"name":.*?[^\\]",' | grep -cP "\"$1\"")
 
 if [ $result -gt 0 ]; then
     echo "Tag already exists on remote"
@@ -37,14 +34,10 @@ fi
 
 echo "Tagging for '$1'"
 
-git tag $1-retail
-git tag $1-classic
-git tag $1-bcc
+git tag $1
 
 echo "Pushing tags for '$1'"
 
-git push origin $1-retail
-git push origin $1-classic
-git push origin $1-bcc
+git push origin $1
 
 echo "Done"
